7ec2b596bb96a70550cfed45a39d3dcf7b37c430
Otlc21 <lc.lucas98@hotmail.com>
2025-01-21 21:40:12 -0300

    ajustando o front

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace App.Controllers
             return View();
         }
 
+        public IActionResult Term()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
